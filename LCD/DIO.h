dfb3b270c20e55410fc1da5b59c0446fe5b678ab
Ahmed-Magdyy <64546783+Ahmed-Magdyy@users.noreply.github.com>
2020-04-30 16:51:07 +0200

    Add files via upload

---
+++ LCD/DIO.h
@@ -0,0 +1,53 @@
+/*
+ * DIO.h
+ *
+ *  Created on: Aug 30, 2019
+ *      Author: Ahmed Magdy
+ */
+
+#ifndef DIO_H_
+#define DIO_H_
+#define HIGHV 1
+#define LOWV 0
+typedef struct  {
+	u8 y;
+	u8 status;
+}read;
+typedef enum {
+	PA=0,
+	PB,
+	PC,
+	PD
+}DIOPortID_t;
+typedef enum {
+	PIN0=0,
+	PIN1,
+	PIN2,
+	PIN3,
+	PIN4,
+	PIN5,
+	PIN6,
+	PIN7
+}DIOPinID_t;
+typedef enum {
+	OUT=1,
+	INFREE,
+	INPULLUP
+}DIODirection_t;
+typedef enum {
+	OK=0,
+	NOK
+}DIOStatus_t;
+DIOStatus_t DIO_SetPinDirection (DIOPortID_t port ,DIOPinID_t pin,DIODirection_t dir);
+DIOStatus_t DIO_SetPortDirection (DIOPortID_t port ,u8 mask);
+void DIO_WritePin(DIOPortID_t port,DIOPinID_t pin,u8 value);
+void DIO_WritePort(DIOPortID_t port,u8 value);
+read DIO_ReadPin(DIOPortID_t port,DIOPinID_t pin);
+u8 DIO_ReadPort(DIOPortID_t port);
+
+
+
+
+
+
+#endif /* DIO_H_ */
---
+++ LCD/LCD_int.h
@@ -0,0 +1,33 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Sep 6, 2019
+ *      Author: Ahmed Magdy
+ */
+
+#ifndef LCD_INT_H_
+#define LCD_INT_H_
+typedef enum {
+	OKAY=0,
+	NOKAY
+}LCD_Status;
+#define CMND_FUNC_SET_CMND 0x38
+#define CMND_DISPLAY_ON_CURSOR_OFF 0x0C
+#define CMND_DISPLAY_CLR 0x01
+#define CMND_ENTRY_MODE_SET 0x06
+#define LCD_CONTROL_PORT PB
+#define LCD_DATA_PORT PD
+#define LCD_RS_PIN PIN0
+#define LCD_RW_PIN PIN1
+#define LCD_E_PIN PIN2
+#define LINE1_BASE 0x80
+#define LINE2_BASE 0xC0
+#define SHIFT_RIGHT 0x1B
+#define CGRAM 0x40
+void LCD_init(void);
+LCD_Status LCD_send_cmnd(u8 cmnd);
+LCD_Status LCD_send_char(u8 data);
+LCD_Status LCD_GO_TO_X_Y(u8 line,u8 y);
+LCD_Status LCD_send_pattern(u8 line , u8 y);
+
+#endif /* LCD_INT_H_ */
